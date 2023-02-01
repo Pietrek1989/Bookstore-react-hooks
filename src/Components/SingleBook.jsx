1b68aa039453555fb651db4dadd468ea4dd0bdf3
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-02-01 16:31:54 +0100

    added selection badge

--- src/Components/SingleBook.jsx
+++ src/Components/SingleBook.jsx
@@ -19,7 +19,7 @@ class SingleBook extends Component {
     render() {
     return (
         <Col xs={6} sm={6} md={4} lg={3} xl={2} key={this.props.currentBook.asin} >
-        <Card className={this.state.active ? "mb-2 red" : "mb-2"}   onClick={this.state.active? this.setFalse : this.setTrue}>
+        <Card className={this.state.active ? "my-2 red" : "my-2"}   onClick={this.state.active? this.setFalse : this.setTrue}>
             <Card.Img className="card-image"  variant="top" src={this.props.currentBook.img} />
             {/* <Card.Body className="card-body">
                 <Card.Title className="card-title d-flex align-items-center font-italic">{this.props.currentBook.title}</Card.Title>
@@ -30,6 +30,8 @@ class SingleBook extends Component {
             <Card.Footer className="card-footer">
                 <small className="badge badge-success ">${this.props.currentBook.price}</small>
             </Card.Footer>
+            <small className={this.state.active ? "badge" : "d-none"}><i class="bi bi-check-lg"></i></small>
+
         </Card>
     </Col>
     )
